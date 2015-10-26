a83310ec49b2d08f88dcaf56becb76dd4cc3f0ff
sir_nacnud <duncanc4@gmail.com>
2015-10-26 13:14:13 +0100

    Present UIImagePickerController after UIActionSheet has been dismissed
    When on iPad with iOS 8 or later, UIActionSheet will use UIAlertController. We then have to make sure the UIAlertController is dismissed before we present the UIImagePickerController, or it won't be presented.

--- Classes/CTFeedbackViewController.m
+++ Classes/CTFeedbackViewController.m
@@ -592,7 +592,7 @@ static NSString * const ATTACHMENT_FILENAME = @"screenshot.jpg";
     [self presentViewController:controller animated:YES completion:nil];
 }
 
-- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex {
     UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
     if (buttonIndex == 0) {
         // camera
